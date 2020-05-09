70f86203930e4105ce505c1719d3bc55ac31991d
dpgian <dpgian@gmail.com>
2020-05-09 12:40:16 +0100

    Build for deployment

---
+++ build/precache-manifest.c12fff68af0d41a966b35c260f06c687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75241a7c276feacda238e6d39437fe49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24a4600d8c79be5fea7",
+    "url": "/static/js/2.3e4da069.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e4da069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754c10cf25f38b3da7cc",
+    "url": "/static/js/main.4770d11c.chunk.js"
+  },
+  {
+    "revision": "83b485ccb7be819e13c1",
+    "url": "/static/js/runtime-main.0edee1d8.js"
+  }
+]);
